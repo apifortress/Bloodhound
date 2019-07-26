57d3ef34d3fcf36155e319dc0cbdc38ecc7a8399
theirish81 <theirish81@gmail.com>
2019-07-26 12:00:59 +0200

    Added docker start script. Java_opts can now be parametrized with env variables. Fixed bug in java_opts.sh

--- bin/java_opts.sh
+++ bin/java_opts.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-export JAVA_OPTS="-Xms512m -Xmx1024m"
+if [ -z "${memorySettings}" ]; then
+        echo "Memory settings not provided. Defaulting to -Xms256m -Xmx512m";
+        export memorySettings="-Xms256m -Xmx512m";
+else
+        echo "Memory settings provided: ${memorySettings}";
+fi;
+
+JAVA_OPTS="-server ${memorySettings}"
+echo "Options string: ${CATALINA_OPTS}"
