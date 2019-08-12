53bd0f4cef7aa20cdd4492f70ff1f2efdbae0d88
theirish81 <theirish81@gmail.com>
2019-08-12 11:52:00 +0200

    documenting a trait

--- src/main/scala/com/apifortress/afthem/config/TConfigLoader.scala
+++ src/main/scala/com/apifortress/afthem/config/TConfigLoader.scala
@@ -1,11 +1,27 @@
 package com.apifortress.afthem.config
 
+/**
+  * Trait for config loaders
+  */
 trait TConfigLoader {
 
+  /**
+    * Loads backend definitions
+    * @return an instance of Backends
+    */
   def loadBackends() : Backends
 
+  /**
+    * Loads a flow definition
+    * @param id the ID of the flow
+    * @return an instance of Flow
+    */
   def loadFlow(id : String) : Flow
 
+  /**
+    * Loads implementers
+    * @return an instance of Implementers
+    */
   def loadImplementers() :  Implementers
 
 }
