15c4de21efa29f0d100db7bb2f7c62387b02cbee
theirish81 <theirish81@gmail.com>
2019-09-11 15:53:08 +0200

    with query

--- src/test/scala/com/apifortress/afthem/actors/filters/ApiKeyFilterActorTests.scala
+++ src/test/scala/com/apifortress/afthem/actors/filters/ApiKeyFilterActorTests.scala
@@ -11,18 +11,26 @@ import com.apifortress.afthem.messages.beans.AfthemResult
 import com.apifortress.afthem.messages.{BaseMessage, WebParsedRequestMessage}
 import org.apache.commons.io.IOUtils
 import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.{Before, Test}
 
 
 class ApiKeyFilterActorTests {
 
-  @Test
-  def testActor() : Unit = {
+
+  var path : String = null
+
+  @Before
+  def initFile() : Unit = {
     YamlConfigLoader.SUBPATH="etc.test"
     val file = File.createTempFile("afthem","test")
     val fileWriter = new FileWriter(file)
     IOUtils.write("api_keys:\n- api_key: ABC123\n  app_id: John Doe\n  enabled: true",fileWriter)
     fileWriter.close()
+    path = file.getAbsolutePath
+  }
+
+  @Test
+  def testActorWithHeader() : Unit = {
 
     val result = new AfthemResult(){
       override def setData(exception: Exception, status: Int, contentType: String, message: BaseMessage): Unit = {
@@ -37,10 +45,41 @@ class ApiKeyFilterActorTests {
         probe.ref ! message
       }
       override def getPhase(message: BaseMessage): Phase = {
-        new Phase("abc","next",List.empty[String],Map("in"->"header","name"->"x-key","filename"->file.getAbsolutePath))
+        new Phase("abc","next",List.empty[String],Map("in"->"header","name"->"x-key","filename"->path))
       }
     }))
     val message = new WebParsedRequestMessage(TestData.createWrapper(),null,null,result)
+
+    actor ! message
+    val request = probe.expectMsgClass(classOf[WebParsedRequestMessage])
+    assertEquals("ABC123",request.request.getHeader("x-key"))
+    message.request.setHeader("x-key","111")
+    actor ! message
+    Thread.sleep(500)
+    system.terminate()
+  }
+
+  @Test
+  def testActorWithQuery() : Unit = {
+    val result = new AfthemResult(){
+      override def setData(exception: Exception, status: Int, contentType: String, message: BaseMessage): Unit = {
+        assertEquals(401,status)
+      }
+    }
+
+    implicit val system = ActorSystem()
+    val probe = TestProbe()
+    val actor = system.actorOf(Props(new ApiKeyFilterActor("abc") {
+      override def tellNextActor(message: BaseMessage) : Unit = {
+        probe.ref ! message
+      }
+      override def getPhase(message: BaseMessage): Phase = {
+        new Phase("abc","next",List.empty[String],Map("in"->"query","name"->"x-key","filename"->path))
+      }
+    }))
+    val wrapper = TestData.createWrapper()
+    wrapper.setURL("http://foo.com/bar?x-key=ABC123")
+    val message = new WebParsedRequestMessage(wrapper,null,null,result)
 
     actor ! message
     val request = probe.expectMsgClass(classOf[WebParsedRequestMessage])
