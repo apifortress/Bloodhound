82f261e1c60af8cf41691e4818963d8de335b152
theirish81 <theirish81@gmail.com>
2019-09-29 09:56:16 +0200

    introducing the timeout to the probe

--- src/main/scala/com/apifortress/afthem/actors/probing/ProbeHttpActor.scala
+++ src/main/scala/com/apifortress/afthem/actors/probing/ProbeHttpActor.scala
@@ -20,50 +20,73 @@ import akka.actor.Actor
 import com.apifortress.afthem.AfthemHttpClient
 import com.apifortress.afthem.routing.{RoutedUrl, TUpstreamHttpRouter}
 import org.apache.http.HttpResponse
+import org.apache.http.client.config.RequestConfig
 import org.apache.http.concurrent.FutureCallback
 import org.apache.http.util.EntityUtils
 import org.slf4j.LoggerFactory
 
+import scala.concurrent.duration.Duration
+
+
+object ProbeHttpActor {
+  /**
+    * The logger
+    */
+  val log = LoggerFactory.getLogger(this.getClass)
+}
 /**
   * The actor executing the probe tasks
   */
 class ProbeHttpActor extends Actor {
 
-  /**
-    * The logger
-    */
-  private val log = LoggerFactory.getLogger(this.getClass)
+
 
   override def receive: Receive = {
     case msg : TUpstreamHttpRouter => {
-      log.debug("Received router for probes")
+      ProbeHttpActor.log.debug("Received router for probes")
       msg.urls.foreach(it => self ! it)
     }
     case msg : RoutedUrl => {
-      log.debug("Probing "+msg.url)
+      ProbeHttpActor.log.debug("Probing "+msg.url)
       val baseRequest = AfthemHttpClient.createBaseRequest(msg.probe.method,msg.url+msg.probe.path)
-      AfthemHttpClient.execute(baseRequest, new FutureCallback[HttpResponse] {
-        override def completed(t: HttpResponse): Unit = {
-          if(t.getStatusLine.getStatusCode == msg.probe.status) {
-            log.debug("Status code was the one expected, voting up for "+msg.url)
-            msg.addStatus(true)
-          }
-          else {
-            log.debug("Status code was not the one expected, voting down for "+msg.url)
-            msg.addStatus(false)
-          }
-          val entity = t.getEntity
-          if(entity != null)
-            EntityUtils.consume(entity)
-        }
-
-        override def failed(e: Exception): Unit = {
-          log.debug("Request failed for "+msg.url)
-          msg.addStatus(false)
-        }
-
-        override def cancelled(): Unit = {}
-      })
+      val timeoutMillis = Duration(msg.probe.timeout).toMillis.toInt
+      val requestConfig = RequestConfig.custom().setConnectTimeout(timeoutMillis)
+                                                  .setSocketTimeout(timeoutMillis)
+                                                  .setRedirectsEnabled(true)
+                                                  .setMaxRedirects(5).build()
+      baseRequest.setConfig(requestConfig)
+      AfthemHttpClient.execute(baseRequest, new ProbeFutureCallback(msg))
     }
   }
+}
+
+/**
+  * Probe future
+  * @param msg the routedUrl
+  */
+class ProbeFutureCallback(val msg : RoutedUrl) extends FutureCallback[HttpResponse] {
+
+  override def completed(t: HttpResponse): Unit = {
+    // If the status code is the one expected
+    if(t.getStatusLine.getStatusCode == msg.probe.status) {
+      ProbeHttpActor.log.debug("Status code was the one expected, voting up for " + msg.url)
+      msg.addStatus(true)
+    }
+    // If the status code is not the one expected
+    else {
+      ProbeHttpActor.log.debug("Status code was not the one expected, voting down for "+msg.url)
+      msg.addStatus(false)
+    }
+    val entity = t.getEntity
+    if(entity != null)
+      EntityUtils.consume(entity)
+  }
+
+  override def failed(e: Exception): Unit = {
+    ProbeHttpActor.log.debug("Request failed for "+msg.url)
+    msg.addStatus(false)
+  }
+
+  override def cancelled(): Unit = {}
+
 }
