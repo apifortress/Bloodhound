28a944627ba09266ec8c11cf1f3313835545f13f
theirish81 <theirish81@gmail.com>
2020-02-06 15:36:52 +0100

    secondary port

--- src/main/scala/com/apifortress/afthem/Main.scala
+++ src/main/scala/com/apifortress/afthem/Main.scala
@@ -20,18 +20,64 @@ import java.util.Date
 
 import com.apifortress.afthem.actors.AppContext
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.web.server.{WebServerFactory, WebServerFactoryCustomizer}
+import org.springframework.context.annotation.{Bean, ComponentScan}
+import org.apache.catalina.connector.Connector
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 
+/**
+  * The Main class
+  */
 @SpringBootApplication
 @ComponentScan
-class Main
+class Main {
 
+  /**
+    * @return a Tomcat customizer
+    */
+  @Bean
+  def containerCustomizer() : WebServerFactoryCustomizer[TomcatServletWebServerFactory] = {
+    return new TomcatWebserverCustomizer()
+  }
+
+}
+
+/**
+  * Tomcat customizer that adds a secondary port, if needed
+  */
+class TomcatWebserverCustomizer extends WebServerFactoryCustomizer[TomcatServletWebServerFactory] {
+
+  /**
+    * Configuration parameter for secondary port
+    */
+  @Value("${server.secondary_port:-1}") var secondaryPort : Integer = null
+
+  override def customize(factory: TomcatServletWebServerFactory): Unit = {
+    if(secondaryPort > -1) {
+      val connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+      connector.setPort(secondaryPort)
+      factory.addAdditionalTomcatConnectors(connector)
+    }
+  }
+
+}
+
+/**
+  * Companion object for Main
+  */
 object Main {
 
+  /**
+    * Logger
+    */
   private val log = LoggerFactory.getLogger(classOf[Main])
 
+  /**
+    * Bootstrap time
+    */
   var bootstrapTime : Date = _
 
   def main(args: Array[String]): Unit = {
